aad632873f4e47b1c484ac2d8408b1f704e5b6f2
Your Name <janu.patodiya123@gmail.com>
2022-02-28 12:53:31 +0530

    [2/28/2022]

---
+++ android/app/src/main/kotlin/com/example/consultancies/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.consultancies
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/resourses.dart
@@ -0,0 +1,9 @@
+///Images
+String splash_image1 = 'assets/splash1.png';
+String splash_image2 = 'assets/splash2.png';
+String screen_logo = 'assets/screen_logo.png';
+
+///Colors
+int splash_bg_color = 0XFFE5E5E5;
+int font_color = 0xFf243443;
+int button_color = 0xFF0B0B45;
